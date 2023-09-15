1f02fd47190653cdfeec95c37cd0a8391fdb11fe
Abhishek <maurya4122002@gmail.com>
2023-09-16 00:40:53 +0530

    fix bug in naigation bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
         },
     ]
   return (
-    <div className=" flex justify-between items-center w-full h-20 px-4 text-white bg- fixed">
+    <div className=" flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
        <div>
             <h1 className=" text-blue-500 shadow-md  text-5xl font-signature ml-2 ">Abhishek Maurya</h1>
         </div>
@@ -43,7 +43,7 @@ const NavBar = () => {
             {nav ? <FaTimes size={30}/>: <FaBars size={30}/>}
         </div>
 
-        {nav &&(<ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b text-gray-500">
+        {nav &&(<ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
         {links.map(({id,link}) =>(
              <li key={id} className="px-4 cursor-pointer capitalize py-6 text-4xl">
                <Link onClick={()=>setNav(!nav)} to={link} smooth duration={500} >{link}</Link>
